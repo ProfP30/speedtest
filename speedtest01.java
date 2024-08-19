6cf7f097eca2282e13f02c874553d05aa6baeb19
Paul S <5628708-ProfP303@users.noreply.gitlab.com>
2024-08-19 18:49:33 +0200

    Added some improvements.

--- speedtest01.java
+++ speedtest01.java
@@ -1,8 +1,8 @@
-public class Main {
+public class speedtest01 {
     public static void main(String[] args) {
         int last_number = 0;
         // Loop from 1 to 1.000.000.000
-        for (int i = 1; i <= 1000000000; i++) {
+        for (int i = 0; i < 1_000_000_000; i++) {
             last_number = i;
         }
         System.out.println("last_number: " + last_number );
--- speedtest01.numpy.py
+++ speedtest01.numpy.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-# Create a numpy array with 1000 elements (0 to 999)
+# Create a numpy array
 end_loop = np.arange(1_000_000_000)
 
 def main():
     # Loop through the numpy array
     last_number: int
     for i in end_loop:
-        # Your code goes here
         last_number = i
 
     print(last_number)
