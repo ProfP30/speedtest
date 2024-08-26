cfaa73d5e865a9ae0e7bb6d5d2da88604aff3cee
Paul S <5628708-ProfP303@users.noreply.gitlab.com>
2024-08-26 11:34:45 +0200

    Using unsigned integer in GO example

--- speedtest01.go
+++ speedtest01.go
@@ -5,12 +5,12 @@ package main
 
 import "fmt"
 
-const END_LOOP int = 1_000_000_000
+const END_LOOP uint = 1_000_000_000
 
 func main() {
-	var last_number int
+	var last_number uint
 
-	for i := 0; i < END_LOOP; i++ {
+	for i := uint(0); i < END_LOOP; i++ {
 		last_number = i
 	}
 	fmt.Printf("last_number=%d\n", last_number)
