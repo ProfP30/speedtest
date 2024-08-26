18631cf0187c63bf9c8a3a714e4e18a35f5aae25
Paul S <5628708-ProfP303@users.noreply.gitlab.com>
2024-08-26 12:48:31 +0200

    Redefine global constant in GO

--- speedtest01.go
+++ speedtest01.go
@@ -5,13 +5,13 @@ package main
 
 import "fmt"
 
-const END_LOOP uint = 1_000_000_000
+const END_LOOP = 1_000_000_000 // 1. Mrd.
 
 func main() {
 	var last_number uint
 
 	for i := uint(0); i < END_LOOP; i++ {
-		last_number = i
+		last_number++
 	}
-	fmt.Printf("last_number=%d\n", last_number)
+	fmt.Printf("GO:last_number=%d\n", last_number)
 }
