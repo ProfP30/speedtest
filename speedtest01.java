03dfa488b5319ce30c40b978d57607c50497151b
Paul S <5628708-ProfP303@users.noreply.gitlab.com>
2024-08-26 12:54:24 +0200

    Print language in Java

--- speedtest01.java
+++ speedtest01.java
@@ -4,8 +4,8 @@ public class speedtest01 {
         // Loop from 1 to 
         int last_number = 0;
         for (int i = 0; i < END_LOOP; i++) {
-            last_number = i;
+            last_number++;
         }
-        System.out.println("last_number=" + last_number);
+        System.out.println("Java:last_number=" + last_number + "\n");
     }
 }
