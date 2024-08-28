1098a92a6daa56ba776aa0e7006c2ff9b003661b
ProfP30 <p.salajean@gmx.de>
2024-08-28 15:44:12 +0200

    Initialized last_number with 0 in C++

--- speedtest01.cpp
+++ speedtest01.cpp
@@ -6,7 +6,7 @@
 #define END_LOOP 1000000000 // 1 Mrd.
 
 int main() {
-    unsigned int last_number;
+    unsigned int last_number=0;
 
     for (unsigned int i = 0; i < END_LOOP; i++) {
         last_number++;
